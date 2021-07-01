9f6716d8dff7344bd847974ac1307eaa68932f7b
John Chichester <jchichester7@gmail.com>
2021-06-30 20:20:32 -0400

    start profile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    <h1>Here is your profile:</h1>
-    <h2>{{ student }}</h2>
+    <div class="card mb-4">
+      <div class="card-header">
+        <h1>{{ student.first_name + " " + student.last_name }}</h1>
+      </div>
+      <div class="card-body">
+        This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things
+        modifying the look and feel of this default card example.
+      </div>
+    </div>
+
+    <!-- Basic Card Example -->
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+      </div>
+      <div class="card-body">
+        The styling for this basic card example is created by using default Bootstrap utility classes. By using utility
+        classes, the style of the card component can be easily modified with no need for any custom CSS!
+      </div>
+    </div>
   </div>
 </template>
 
